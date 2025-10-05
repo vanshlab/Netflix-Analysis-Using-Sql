fca0f62aa4173bcd97318c195a30deb021d81ae8
vanshlab <vanshchaudhary1825@gmail.com>
2025-10-05 17:05:35 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -9,8 +9,7 @@
 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
 8. List all TV shows with more than 5 seasons
 9. Count the number of content items in each genre
-10.Find each year and the average numbers of content release in India on netflix. 
-return top 5 year with highest avg content release!
+10.Find No of movies Anupam Kher works
 11. List all movies that are documentaries
 12. Find all content without a director
 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
